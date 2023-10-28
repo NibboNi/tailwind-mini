545f8f60f3c41665b30d886c4a45d01d22c5ece3
Nicolás <131214845+NibboNi@users.noreply.github.com>
2023-10-28 01:29:57 -0600

    Update Mobile menu

--- js/app.js
+++ js/app.js
@@ -2,15 +2,18 @@ const navLinks = document.querySelector("#navLinks");
 const mobileMenu = document.querySelector("#mobileMenu");
 
 mobileMenu.addEventListener("click", () => {
+  navLinks.classList.remove("bottom-full");
   if (mobileMenu.classList.contains("open-menu")) {
     mobileMenu.classList.add("close-menu");
     mobileMenu.classList.remove("open-menu");
-    navLinks.classList.add("hidden");
-    navLinks.classList.remove("flex");
+
+    navLinks.classList.remove("show");
+    navLinks.classList.add("hide");
   } else {
     mobileMenu.classList.add("open-menu");
     mobileMenu.classList.remove("close-menu");
-    navLinks.classList.remove("hidden");
-    navLinks.classList.add("flex");
+
+    navLinks.classList.add("show");
+    navLinks.classList.remove("hide");
   }
 });
