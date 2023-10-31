e8d533ed20942d224cbc411bca0f01c30caef663
Nicolás <131214845+NibboNi@users.noreply.github.com>
2023-10-30 20:28:04 -0600

    Add Testimonial grid example

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
+  screens: {
+    sm: "480px",
+    md: "768px",
+    lg: "976px",
+    xl: "1440px",
+  },
   theme: {
-    fontFamily: {
-      sans: ["Mulish", "sans-serif"],
-      mono: ["Rokkitt", "monospace"],
-    },
     extend: {
       animation: {
         "ping-slow": "ping 1.2s linear infinite",
+      },
+      fontFamily: {
+        sans: ["Mulish", "sans-serif"],
+        mono: ["Rokkitt", "monospace"],
       },
     },
   },
